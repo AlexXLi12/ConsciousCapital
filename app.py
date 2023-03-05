e5e5c9c13a4037f2b9fc50a8f75ec3c5a12d3947
Alex Li <alexxuanqili@gmail.com>
2023-03-04 19:46:57 -0500

    completed all form fields

--- app.py
+++ app.py
@@ -14,12 +14,12 @@ def form():
 
 @app.route('/submit_form', methods=['POST'])
 def submit_form():
-    print(request.form.getlist('sectors'))
+    print(request.form)
     #insert function conversion
     #temporary (sample form results that match input for model.py)
     temporaryFormReponseCorrectFormat = np.array([9, 3, 3, 2, 4, 7, 1, 0.1, 1, 1, 2, 2, 1, 0.1, 0.1, 1, 1]) # ENV, SOC, GOV, CON, FEM, MIN, 11 Sectors
     answer = model.runModel(temporaryFormReponseCorrectFormat)
     print(answer)
-    return render_template('submit_form.html', )
+    return render_template('submit_form.html')
 if __name__ == '__main__':
     app.run()
